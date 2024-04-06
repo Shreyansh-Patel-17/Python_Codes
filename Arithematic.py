c7eb0418318e842fec847f28cbf44cd6b21b98ab
Shreyansh-Patel-17 <kingblood452@gmail.com>
2024-04-06 19:33:42 +0530

    Arithematic operations in python added

---
+++ Arithematic.py
@@ -0,0 +1,21 @@
+#To take user input
+x = input("Enter the first number: ")
+y = input("Enter the second number: ")
+
+#To add 2 integers
+print("The value of ",x ,"+",y ,"is",int(x) + int(y),".")
+
+#To subtract one integer from another
+print("The value of ",x ,"-",y ,"is",int(x) - int(y),".")
+
+#To multiply two numbers
+print("The value of ",x ,"*",y ,"is",int(x) * int(y),".")
+
+#To divide one number by another with no remainder.
+print("The value of ",x ,"/",y ,"is",int(x) / int(y),".")
+
+#To find the result of a number raised to power of another number.
+print("The value of ",x ,"to the power",y ,"is",int(x) ** int(y),".")
+
+#To find the remainder of a division (i.e., what remains after one number has been divided by another).
+print("The remainder of ",x ,"/",y ,"is",int(x) % int(y),".")
